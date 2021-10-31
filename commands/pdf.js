7b5e9bc2b8cc5c54eb02f19c465bdc4596f3af28
Andriy M Etcheverry <andriy.etcheverry@gmail.com>
2021-11-01 00:51:40 +0400

    added a bit of visible logging in for !pdf

--- commands/pdf.js
+++ commands/pdf.js
@@ -5,7 +5,7 @@ import * as util from '../utilities/functions.js';
 import config from '../config.json'
 import buildLogger from '../utilities/build-logger.js';
 
-function run(message) {
+async function run(message) {
     var logger = buildLogger();
     var args = message.content.split(' ');
 
@@ -13,7 +13,7 @@ function run(message) {
     if (!util.validator.isURL(args[1]) && args[1] != 'login') return message.channel.send("Usage: `!pdf https://www.chegg.com/answerpagelink`");
     else if (args[1] == 'login' && util.validator.isURL(args[2])) {
         message.channel.send('Opening browser to specified page. Please login to your account using your credentials, then close the window');
-        login(args[2], logger).then(() => {
+        login(args[2]).then(() => {
             logger.warn('Browser opened successfully')
         }).catch ((err) => {logger.error(err)})
         return
@@ -23,16 +23,19 @@ function run(message) {
 
     logger.info();
     logger.info('====PAGE TO PDF V2====');
+    var msgRef = await message.channel.send('```\n====PAGE TO PDF V2====\n```');
+
+    //logger.warn(msgRef.content);
     logger.info();
 
-    puppet(url, logger).then(() => {
+    puppet(args[1], msgRef, logger).then(() => {
         message.channel.send({
             files: ['./pdfs/chegg.pdf']
         });
         logger.info(`PDF sent in discord`);
     })
     .catch((err) => {
-        logger.error(`Some error occurred trying to convert ${url} to PDF`);
+        logger.error(`Some error occurred trying to convert ${args[1]} to PDF`);
         logger.error(err);
     })        
 }
@@ -58,7 +61,7 @@ function login (url) {
     })
 }
 
-function puppet(url, logger) {
+function puppet(url, msgRef, logger) {
     return new Promise(async (resolve, reject) => {
 
         const browser = await puppeteer.launch({
@@ -68,19 +71,24 @@ function puppet(url, logger) {
             userDataDir: 'C:/userData'
         });
 
+        msgRef = await msgRef.edit(msgRef.content.substring(0, msgRef.content.length-3) + "Starting browser process\n```")
+
         try {
             const page = await browser.newPage()
-            await page.goto(url, { waitUntil: 'domcontentloaded' })
+            await page.goto(url, { waitUntil: 'domcontentloaded' });
             
             await page.pdf({ path: 'pdfs/chegg.pdf', format: 'a4' });
     
-            await logger.info(`PDF created in answers directory`);
-    
-            await logger.info(`Closing browser process...`)
+            await logger.info('PDF created in answers directory');
+            msgRef = await msgRef.edit(msgRef.content.substring(0, msgRef.content.length-3) + 'PDF created in answers directory\n```');
+
+            await logger.info(`Closing browser process...`);
+            msgRef = await msgRef.edit(msgRef.content.substring(0, msgRef.content.length-3) + 'Closing browser process...\n```');
             await util.sleep(0.5);
-            await browser.close()
+            await browser.close();
             
             await logger.info(`Browser process closed`);
+            msgRef = await msgRef.edit(msgRef.content.substring(0, msgRef.content.length-3) + 'Browser process closed\n```');
             resolve()
         } catch (error) {
             reject(error)
